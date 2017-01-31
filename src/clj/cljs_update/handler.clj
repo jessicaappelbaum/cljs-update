8c0301b38ecf6e02a8082ee42d934411b6dabea3
Jessica Appelbaum <jessica.appelbaum@thinktopic.com>
2017-01-31 15:46:59 -0700

    [handler & project.clj] added semantic to project

--- src/clj/cljs_update/handler.clj
+++ src/clj/cljs_update/handler.clj
@@ -17,7 +17,9 @@
    [:meta {:charset "utf-8"}]
    [:meta {:name "viewport"
            :content "width=device-width, initial-scale=1"}]
-   (include-css (if (env :dev) "/css/site.css" "/css/site.min.css"))])
+   (include-css
+     (if (env :dev) "/css/site.css" "/css/site.min.css")
+     "//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css")])
 
 (defn loading-page []
   (html5
@@ -30,7 +32,7 @@
 (defroutes routes
   (GET "/" [] (loading-page))
   (GET "/about" [] (loading-page))
-  
+
   (resources "/")
   (not-found "Not Found"))
 
